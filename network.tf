69a0f448ad41ebe820332f9b5c2292962d9fcdef
Surendran Duraisamy <surendran@Surendrans-MacBook-Pro.local>
2024-07-15 23:53:03 +0530

    rt association

--- network.tf
+++ network.tf
@@ -8,8 +8,9 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id     = aws_subnet.public_subnet2.id
 }
 
 resource "aws_subnet" "public_subnet1" {
@@ -62,4 +63,49 @@ resource "aws_subnet" "secure_subnet2" {
   tags = {
     Name = format("%s-secure-subnet-6", var.prefix)
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_eip" "elastic_ip" {
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
+
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "rt_public_subnet1" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet1.id
+}
+
+resource "aws_route_table_association" "rt_public_subnet2" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet2.id
+}
+
+resource "aws_route_table_association" "rt_private_subnet1" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet1.id
+}
+
+resource "aws_route_table_association" "rt_private_subnet2" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet2.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vpc_details" {
-  description = "vpc details"
+  description = "vpc details created by dsuren"
   value       = aws_vpc.vpc.id
 }
