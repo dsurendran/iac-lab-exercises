34ed328b176d210b60fe8898c1b81c01ff599d61
Surendran Duraisamy <surendran@Surendrans-MacBook-Pro.local>
2024-07-17 23:11:43 +0530

    week 2 - s3 bucket and dynamodb table

---
+++ backend_support/variables.tf
@@ -0,0 +1,11 @@
+
+variable "region" {
+  type        = string
+  description = "Region to deploy iac solution"
+  default     = "ap-south-1"
+}
+
+variable "prefix" {
+  type        = string
+  description = "s3 prefix"
+}
